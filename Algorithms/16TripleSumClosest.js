ba450e90a599e03a5df0174311f80e5c87c687d9
王洁 <jiewang@lagou.com>
2017-03-31 18:32:14 +0800

    q16

--- Algorithms/16TripleSumClosest.js
+++ Algorithms/16TripleSumClosest.js
@@ -1,45 +1,46 @@
 /**
  * @param {number[]} nums
- * @return {number[][]}
+ * @param {number} target
+ * @return {number}
  */
-var threeSum = function(nums) {
+var threeSumClosest = function(nums, target) {
   nums.sort(function(a, b) {
     if (a < b) { return -1; }
     else if (a > b) { return 1; }
     else { return 0; }
   });
 
-  var result = [];
-
+  var result = 0;
+  var minInset = -1;
+  
   var i = 0;
-  while (i < nums.length) {
+  while (i < nums.length) {    
     var num = nums[i];
     var prevIndex = i + 1, nextIndex = nums.length - 1;
 
     while (prevIndex < nextIndex && nextIndex < nums.length) {
       var prevNum = nums[prevIndex], nextNum = nums[nextIndex];
       
-      if (prevNum + nextNum + num < 0) {
-        while (nums[prevIndex] === prevNum && prevIndex < nextIndex) {
-          prevIndex++;
-        }
+      var inset = target - (prevNum + nextNum + num);
+      if (inset === 0) {
+        return target;
       }
-      else if (prevNum + nextNum + num > 0) {
+      else if (minInset < 0 || (inset < minInset && inset > -minInset)) {
+        minInset = Math.abs(inset);
+        result = num + prevNum + nextNum;
+      }
+
+      if (inset < 0) {
         while (nums[nextIndex] === nextNum && prevIndex < nextIndex) {
           nextIndex--;  
         }
       }
       else {
-        var triple = [num, prevNum, nextNum];
-        result.push(triple);
-
         while (nums[prevIndex] === prevNum && prevIndex < nextIndex) {
           prevIndex++;
         }
-        while (nums[nextIndex] === nextNum && prevIndex < nextIndex) {
-          nextIndex--;  
-        }
       }
+
     }
 
     while (nums[i] === num && i < nums.length) {
